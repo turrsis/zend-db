db8751c105ac8827b51ffa1497e59bdfd4b0c554
turrsis <turrsis@gmail.com>
2015-07-09 17:40:06 +0300

    architecture 04: optimize tests for Builders

--- test/Sql/Builder/UpdateBuilderTest.php
+++ test/Sql/Builder/UpdateBuilderTest.php
@@ -12,6 +12,9 @@ namespace ZendTest\Db\Sql\Builder;
 use Zend\Db\Sql\Expression;
 use Zend\Db\Sql\TableIdentifier;
 
+/**
+ * @covers Zend\Db\Sql\Builder\sql92\UpdateBuilder
+ */
 class UpdateBuilderTest extends AbstractTestCase
 {
     /**
@@ -25,55 +28,122 @@ class UpdateBuilderTest extends AbstractTestCase
 
     public function dataProvider()
     {
-        return $this->prepareDataProvider([
-            [ //??? testPassingMultipleKeyValueInWhereClause()
-                'sqlObject' => $this->update()
-                                        ->table('table')
-                                        ->set(['fld1' => 'val1'])
-                                        ->where(['id1' => 'val1', 'id2' => 'val2']),
-                'expected'  => [
-                    'sql92' => [
-                        'string'  => 'UPDATE "table" SET "fld1" = \'val1\' WHERE "id1" = \'val1\' AND "id2" = \'val2\'',
-                    ],
-                ],
-            ],
-            [ // testPrepareStatement(), testGetSqlString()
+        return $this->prepareDataProvider(
+            $this->dataProvider_Else(),
+            $this->dataProvider_Set(),
+            $this->dataProvider_SubSelectAndExpressions(),
+            $this->dataProvider_Table(),
+            $this->dataProvider_Where()
+        );
+    }
+
+    public function dataProvider_Set()
+    {
+        return [
+            [
                 'sqlObject' => $this->update()
                                         ->table('foo')
-                                        ->set(['bar' => 'baz', 'boo' => new Expression('NOW()'), 'bam' => null])
-                                        ->where('x = y'),
+                                        ->set([
+                                            'bar'   => 'baz',
+                                            'boo'   => new Expression('NOW()'),
+                                            'bam'   => null,
+                                            'false' => false,
+                                            'true'  => true,
+                                        ]),
                 'expected'  => [
                     'sql92' => [
-                        'string'  => 'UPDATE "foo" SET "bar" = \'baz\', "boo" = NOW(), "bam" = NULL WHERE x = y',
-                        'prepare' => 'UPDATE "foo" SET "bar" = ?, "boo" = NOW(), "bam" = NULL WHERE x = y',
+                        'string'     => 'UPDATE "foo" SET "bar" = \'baz\', "boo" = NOW(), "bam" = NULL, "false" = \'\', "true" = \'1\'',
+                        'prepare'    => 'UPDATE "foo" SET "bar" = ?, "boo" = NOW(), "bam" = NULL, "false" = ?, "true" = ?',
+                        'parameters' => ['bar' => 'baz', 'false' => false, 'true' => true],
                     ],
                 ],
             ],
-            [ // testPrepareStatement() // with TableIdentifier
-                'sqlObject' => $this->update()
-                                        ->table(new TableIdentifier('foo', 'sch'))
-                                        ->set(['bar' => 'baz', 'boo' => new Expression('NOW()'), 'bam' => null])
-                                        ->where('x = y'),
+        ];
+    }
+
+    public function dataProvider_Table()
+    {
+        return [
+            [
+                'sqlObject' => $this->update('foo')->set(['bar' => 'baz']),
                 'expected'  => [
                     'sql92' => [
-                        'string'  => 'UPDATE "sch"."foo" SET "bar" = \'baz\', "boo" = NOW(), "bam" = NULL WHERE x = y',
-                        'prepare' => 'UPDATE "sch"."foo" SET "bar" = ?, "boo" = NOW(), "bam" = NULL WHERE x = y',
+                        'string'  => 'UPDATE "foo" SET "bar" = \'baz\'',
+                        'prepare' => 'UPDATE "foo" SET "bar" = ?',
                     ],
                 ],
             ],
-            [ // testGetSqlStringForFalseUpdateValueParameter()
-                'sqlObject' => $this->update()
-                                        ->table(new TableIdentifier('foo', 'sch'))
-                                        ->set(['bar' => false, 'boo' => 'test', 'bam' => true])
-                                        ->where('x = y'),
+            [
+                'sqlObject' => $this->update(new TableIdentifier('foo', 'sch'))->set(['bar' => 'baz']),
                 'expected'  => [
                     'sql92' => [
-                        'string'  => 'UPDATE "sch"."foo" SET "bar" = \'\', "boo" = \'test\', "bam" = \'1\' WHERE x = y',
-                        'prepare' => 'UPDATE "sch"."foo" SET "bar" = ?, "boo" = ?, "bam" = ? WHERE x = y',
+                        'string'  => 'UPDATE "sch"."foo" SET "bar" = \'baz\'',
+                        'prepare' => 'UPDATE "sch"."foo" SET "bar" = ?',
                     ],
                 ],
             ],
-            [ // testCloneUpdate()
+        ];
+    }
+
+    public function dataProvider_SubSelectAndExpressions()
+    {
+        return [
+            [
+                'sqlObject' => $this->update('foo')->set(['x'=>$this->select('foo')]),
+                'expected'  => [
+                    'sql92'     => 'UPDATE "foo" SET "x" = (SELECT "foo".* FROM "foo")',
+                    'MySql'     => 'UPDATE `foo` SET `x` = (SELECT `foo`.* FROM `foo`)',
+                    'Oracle'    => 'UPDATE "foo" SET "x" = (SELECT "foo".* FROM "foo")',
+                    'SqlServer' => 'UPDATE [foo] SET [x] = (SELECT [foo].* FROM [foo])',
+                ],
+            ],
+            [
+                'sqlObject' => $this->update('foo')->set(['x'=>new Expression('?', [$this->select('foo')])]),
+                'expected'  => [
+                    'sql92'     => 'UPDATE "foo" SET "x" = (SELECT "foo".* FROM "foo")',
+                    'MySql'     => 'UPDATE `foo` SET `x` = (SELECT `foo`.* FROM `foo`)',
+                    'Oracle'    => 'UPDATE "foo" SET "x" = (SELECT "foo".* FROM "foo")',
+                    'SqlServer' => 'UPDATE [foo] SET [x] = (SELECT [foo].* FROM [foo])',
+                ],
+            ],
+        ];
+    }
+
+    public function dataProvider_Where()
+    {
+        return [
+            [
+                'sqlObject' => $this->update('foo')->set(['bar' => 'baz'])->where(['x'=>'y']),
+                'expected'  => [
+                    'sql92'     => [
+                        'string'     => 'UPDATE "foo" SET "bar" = \'baz\' WHERE "x" = \'y\'',
+                        'prepare'    => 'UPDATE "foo" SET "bar" = ? WHERE "x" = ?',
+                        'parameters' => ['bar' => 'baz', 'expr1' => 'y'],
+                    ],
+                    'MySql'     => [
+                        'string'     => 'UPDATE `foo` SET `bar` = \'baz\' WHERE `x` = \'y\'',
+                        'prepare'    => 'UPDATE `foo` SET `bar` = ? WHERE `x` = ?',
+                        'parameters' => ['bar' => 'baz', 'expr1' => 'y'],
+                    ],
+                    'Oracle'    => [
+                        'string'     => 'UPDATE "foo" SET "bar" = \'baz\' WHERE "x" = \'y\'',
+                        'prepare'    => 'UPDATE "foo" SET "bar" = ? WHERE "x" = ?',
+                        'parameters' => ['bar' => 'baz', 'expr1' => 'y'],
+                    ],
+                    'SqlServer' => [
+                        'string'     => 'UPDATE [foo] SET [bar] = \'baz\' WHERE [x] = \'y\'',
+                        'prepare'    => 'UPDATE [foo] SET [bar] = ? WHERE [x] = ?',
+                        'parameters' => ['bar' => 'baz', 'expr1' => 'y'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function dataProvider_Else()
+    {
+        return [
+            'clone' => [ // testCloneUpdate()
                 'sqlObject' => function () {
                         $update1 = clone $this->update();
                         $update1->table('foo')
@@ -94,6 +164,6 @@ class UpdateBuilderTest extends AbstractTestCase
                     ],
                 ],
             ],
-        ]);
+        ];
     }
 }
